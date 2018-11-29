c32273179b9e26223646bc8f5d51edd17959ba3e
Jannik <jannik.niedermayer@gmail.com>
2018-11-29 19:31:21 +0100

    JSON

---
+++ src/io/Convertable.java
@@ -0,0 +1,12 @@
+package io;
+
+public interface Convertable {
+
+
+    public static void setScenario(int i){};
+    public static void profitCounter(){};
+    public static void createStartScenario(){};
+
+
+
+}
